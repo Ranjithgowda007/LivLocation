a4db89299e1e1e2e912b2b692cf8f7e6dae6791b
RanjithGowda <136459904+Ranjithgowda007@users.noreply.github.com>
2024-02-28 16:16:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+
+  const [isloc, setIsloc] = useState(false)
+  const [locationData, setLocationData] = useState({
+    latitude: "",
+    longitude: "",
+    city: "",
+    state: "",
+    country: "",
+  });
+  useEffect(() => {
+    function success(position) {
+      console.log(position);
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      // const geoApiurl = `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=${process.env.REACT_APP_GEOAPIFY_API_KEY}`;
+      const geoapiurl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+
+      fetch(geoapiurl)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setLocationData({
+            latitude: latitude,
+            longitude: longitude,
+            city: data.city,
+            state: data.principalSubdivision,
+            country: data.countryName,
+          });
+          setIsloc(true)
+        })
+        .catch((error) => console.error("Error fetching location:", error));
+    }
+
+    function error(error) {
+      console.error("Error retrieving location:", error);
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isloc && (
+      <div>
+          <h1>Address:</h1>
+        <li>
+          Latitude: {locationData.latitude}, longitude: {locationData.longitude}
+        </li>
+        <li>city: {locationData.city}</li>
+        <li>state: {locationData.state}</li>
+        <li>country: {locationData.country}</li>
+      </div>
+
+      )}
     </div>
   );
 }
-
-export default App;
